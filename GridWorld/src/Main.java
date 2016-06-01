a0f4835e6c84b98640d8f950e6c9b18b47765be8
Maria José Ferreira <mjrf85@gmail.com>
2016-06-01 19:45:25 +0100

    Updates on the functions in the Main file. Changes in the Vstar_line function.

--- GridWorld/src/Main.java
+++ GridWorld/src/Main.java
@@ -5,15 +5,18 @@ import java.util.Vector;
 import javax.swing.text.html.parser.ParserDelegator;
 
 public class Main {
+	private static double LoseReward = 0;
+	private static double GoalReward = 0;
+	private static double gamma = 0.95;
 	static Vector<State> vec_States = new Vector<State>();
 	private static Vector<State> Policy_star;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		InitializeGrid();
-		// begin();
+		//begin();
 		PrintStates();
-		// Print(vec_States);
+		//Print(vec_States);
 
 		StartMDP();
 	}
@@ -28,6 +31,10 @@ public class Main {
 		int NOTHING = 5;
 		double prob_next = 0.8;
 		double prob_stay = 0.10;
+
+		LoseReward = -1;
+		GoalReward = 1;
+		gamma = 0.99;
 
 		Vector<Directions> vec_directions = new Vector<Directions>();
 		Directions e = new Directions();
@@ -45,7 +52,7 @@ public class Main {
 		e.setOvershoot(true);
 		vec_directions.add(e);
 		e = new Directions();
-		vec_States.add(new State(1, vec_directions, -0.04));
+		vec_States.add(new State(1, vec_directions, -0.02));
 		vec_directions = new Vector<Directions>();
 
 		// STATE 2 - move left, right and nothing
@@ -67,7 +74,7 @@ public class Main {
 		e.setOvershoot(false);
 		vec_directions.add(e);
 		e = new Directions();
-		vec_States.add(new State(2, vec_directions, -0.04));
+		vec_States.add(new State(2, vec_directions, -0.02));
 		vec_directions = new Vector<Directions>();
 
 		// STATE 3 - move left, right down, and nothing
@@ -89,7 +96,7 @@ public class Main {
 		e.setOvershoot(false);
 		vec_directions.add(e);
 		e = new Directions();
-		vec_States.add(new State(3, vec_directions, -0.04));
+		vec_States.add(new State(3, vec_directions, -0.02));
 		vec_directions = new Vector<Directions>();
 
 		// STATE 4 - move left, down
@@ -105,7 +112,7 @@ public class Main {
 		e.setOvershoot(false);
 		vec_directions.add(e);
 		e = new Directions();
-		vec_States.add(new State(4, vec_directions, 1));
+		vec_States.add(new State(4, vec_directions, GoalReward));
 		vec_directions = new Vector<Directions>();
 
 		// STATE 5 - move up down, and nothing
@@ -127,7 +134,7 @@ public class Main {
 		e.setOvershoot(false);
 		vec_directions.add(e);
 		e = new Directions();
-		vec_States.add(new State(5, vec_directions, -0.04));
+		vec_States.add(new State(5, vec_directions, -0.02));
 		vec_directions = new Vector<Directions>();
 
 		// STATE 6 - move up down, right,and nothing
@@ -155,7 +162,7 @@ public class Main {
 		e.setOvershoot(false);
 		vec_directions.add(e);
 		e = new Directions();
-		vec_States.add(new State(6, vec_directions, -0.04));
+		vec_States.add(new State(6, vec_directions, -0.02));
 		vec_directions = new Vector<Directions>();
 
 		// STATE 7 - move up down, right,and nothing
@@ -177,7 +184,7 @@ public class Main {
 		e.setOvershoot(false);
 		vec_directions.add(e);
 		e = new Directions();
-		vec_States.add(new State(7, vec_directions, -1));
+		vec_States.add(new State(7, vec_directions, LoseReward));
 		vec_directions = new Vector<Directions>();
 
 		// STATE 8 - move up down, right,and nothing
@@ -193,7 +200,7 @@ public class Main {
 		e.setOvershoot(false);
 		vec_directions.add(e);
 		e = new Directions();
-		vec_States.add(new State(8, vec_directions, -0.04));
+		vec_States.add(new State(8, vec_directions, -0.02));
 		vec_directions = new Vector<Directions>();
 
 		// STATE 9 - move up down, right,and nothing
@@ -215,7 +222,7 @@ public class Main {
 		e.setOvershoot(false);
 		vec_directions.add(e);
 		e = new Directions();
-		vec_States.add(new State(9, vec_directions, -0.04));
+		vec_States.add(new State(9, vec_directions, -0.02));
 		vec_directions = new Vector<Directions>();
 
 		// STATE 10 - move up down, right,and nothing
@@ -237,7 +244,7 @@ public class Main {
 		e.setOvershoot(false);
 		vec_directions.add(e);
 		e = new Directions();
-		vec_States.add(new State(10, vec_directions, -0.04));
+		vec_States.add(new State(10, vec_directions, -0.02));
 		vec_directions = new Vector<Directions>();
 
 		// STATE 11 - move up down, right,and nothing
@@ -253,7 +260,7 @@ public class Main {
 		e.setOvershoot(false);
 		vec_directions.add(e);
 		e = new Directions();
-		vec_States.add(new State(11, vec_directions, -0.04));
+		vec_States.add(new State(11, vec_directions, -0.02));
 		vec_directions = new Vector<Directions>();
 
 	}
@@ -269,7 +276,7 @@ public class Main {
 		System.out.println("");
 		System.out.println("V(0)");
 		PrintStates(Vstar_line);
-		// Print(Vstar_line);
+		//Print(Vstar_line);
 		Vector<Double> Vstar = new Vector<Double>();
 
 		Vstar = Vstar_line;
@@ -294,13 +301,14 @@ public class Main {
 				case 4:
 					d_value = "R";
 					break;
-				case -100:
-					d_value = "-100";
-					break;
-				case 10:
-					d_value = "10";
-					break;
 				}
+
+				if (d_value == "" && aux_double == LoseReward) {
+					d_value = "" +LoseReward +"";
+				}else if (d_value == "" && aux_double == GoalReward) {
+					d_value = "" +GoalReward +"";
+				}
+
 				Policy_star.set(i, d_value);
 			}
 			teration++;
@@ -308,25 +316,30 @@ public class Main {
 			System.out.println("");
 			System.out.println("V(" + teration + ")");
 			PrintStates(Vstar_line);
-			// Print(Vstar_line);
+			//Print(Vstar_line);
 		}
 		System.out.println("");
 		PrintStates(Policy_star);
+		//Print(Policy_star);
+/*		System.out.println("");
+		System.out.println("Gamma = " + gamma);*/
 	}
 
-	private static double[] Vstar_line(int i, Vector<Double> v) {
+	private static double[] Vstar_line(int i, Vector<Double> v) {//implementing Q(s,a) = Reward + Gamma * Q(s'a') 
 		// TODO Auto-generated method stub
-		double gamma = 0.95;
+
 		double aux;
 		int policy = 0;
+		aux = vec_States.get(i).getReward(); // if its the last states
 
-		if ((vec_States.get(i).getPosition() == 23) || (vec_States.get(i).getPosition() == 13)) {
-			aux = vec_States.get(i).getReward(); // if its the last states
-			if (vec_States.get(i).getPosition() == 23) {
-				policy = -100;
-			} else {
+		if ((aux == LoseReward) || (aux == GoalReward)) {
+
+
+			/*if (aux == LoseReward) {//negative reward
+			 */				policy = (int) aux;
+			 /*} else {
 				policy = 10;
-			}
+			}*/
 		} else {
 			double max = 0;
 			double maxUp = 0.0, maxDown = 0.0, maxRight = 0.0, maxLeft = 0.0/*, maxNothing = 0.0*/;
@@ -378,94 +391,6 @@ public class Main {
 		return a;
 	}
 
-	private static void StartGame() {
-		// TODO Auto-generated method stub
-		int WinState = 13;// State 13 robot wins
-		int LoseState = 23;// State 23 robot loses
-		int pos = 1;// begin in the 1State
-		Random randomGenerator = new Random();
-		String direction = "";
-		int randomDirection = 0;
-		boolean finished = false;
-		while (finished != true) {
-
-			int randomOvershoot = randomGenerator.nextInt(2);
-
-			int nextpos = pos;
-			Vector<Directions> directions = vec_States.get(pos - 1).getDirections();
-
-			int numdirectionsavailable = directions.size() - 1;
-			randomDirection = randomGenerator.nextInt(numdirectionsavailable);
-
-			boolean isOvershoot = false;
-			nextpos = directions.get(randomDirection).getNextposition();
-
-			System.out.println("S" + pos + " " + directions.get(randomDirection).toString());
-
-			if (pos == WinState) {
-				System.out.println("_______WIN_______");
-			} else if (pos == LoseState) {
-				System.out.print("GAME OVER !!!!!");
-			}
-
-			// MPD(pos);
-
-			finished = ((pos == WinState) || (pos == LoseState));
-			pos = nextpos;
-
-		}
-	}
-
-	private static void MPD(double valueGama) {// Markov Decision Process
-		// TODO Auto-generated method stub
-
-		Vector<Double> V_Pi = new Vector<Double>();
-		Vector<Double> aux = new Vector<Double>();
-		// V_Pi.add(V_Pi(state));
-
-		for (int i = 0; i < vec_States.size(); i++) {// initialize all V(s) = 0;
-			V_Pi.add(0.0);
-		}
-		System.out.println("V(0)");
-		PrintStates(V_Pi);
-
-		int teste = 1;
-		while (teste < 10) {
-			for (int i = 0; i < vec_States.size(); i++) {// next iterations
-				Vector<Directions> direct = vec_States.get(i).getDirections();
-				double reward = vec_States.get(i).getReward();
-				double gama = valueGama;
-				double v_pi = 0.0;
-				double SumProbabilities = 0;
-				for (int j = 0; j < direct.size(); j++) {
-					double direction_probabilty = direct.get(j).getProbability();
-					int nextposition = direct.get(j).getNextposition() - 1;// vector
-					// with
-					// V(s)
-					// starts
-					// in
-					// 0
-					SumProbabilities = SumProbabilities + (direction_probabilty * V_Pi.get(nextposition));
-				}
-
-				v_pi = reward + gama * SumProbabilities;
-
-				v_pi = Math.round(v_pi * 100);
-				v_pi = v_pi / 100;// 2 decimal places
-
-				aux.add(v_pi);
-			}
-
-			V_Pi = new Vector<>();// delete all values from previous iteration
-			V_Pi = aux;// add to V_Pi the values of the new iteration
-			aux = new Vector<>();// delete all values from previous iteration
-			System.out.println("");
-			System.out.println("V(" + teste + ")");
-			PrintStates(V_Pi);
-			teste++;
-		}
-
-	}
 
 	private static void PrintStates(Vector v_Pi) {
 		// TODO Auto-generated method stub
@@ -477,9 +402,28 @@ public class Main {
 			for (int j = 0; j < 7; j++) {
 				pos = st[i][j];
 				if (pos == -1) {
+					System.out.print(String.format("%10s", ""));
+				} else {
+					System.out.print(String.format("%10s", v_Pi.get(pos - 1).toString()));
+				}
+			}
+			System.out.println("");
+		}
+	}
+	
+	private static void PrintStates() {
+		// TODO Auto-generated method stub
+		int[][] st = { { -1, -1, -1, 1, -1, -1, -1 }, { 20, 21, 22, 2, 3, 4, 5 }, { 19, -1, -1, 23, -1, -1, 6 },
+				{ 18, -1, -1, -1, -1, -1, 7 }, { 17, -1, -1, 13, -1, -1, 8 }, { 16, 15, 14, 12, 11, 10, 9 } };
+		int pos;
+
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 7; j++) {
+				pos = st[i][j];
+				if (pos == -1) {
 					System.out.print(String.format("%30s", ""));
 				} else {
-					System.out.print(String.format("%30s", v_Pi.get(pos - 1).toString()));
+					System.out.print(String.format("%30s", vec_States.get(pos - 1).toString()));
 				}
 			}
 			System.out.println("");
@@ -504,43 +448,6 @@ public class Main {
 		}
 	}
 
-	private static Double V_Pi(int state) {
-
-		// TODO Auto-generated method stub
-		Vector<Directions> direct = vec_States.get(state - 1).getDirections();
-		double reward = vec_States.get(state - 1).getReward();
-		double gama = 0.95;
-		double v_pi = 0.0;
-		double SumProbabilities = 0;
-		for (int i = 0; i < direct.size(); i++) {
-			double direction_probabilty = direct.get(i).getProbability();
-			int nextposition = direct.get(i).getNextposition();
-			SumProbabilities = SumProbabilities + direction_probabilty * V_Pi(nextposition);
-		}
-
-		v_pi = reward + gama * SumProbabilities;
-
-		return v_pi;
-	}
-
-	private static void PrintStates() {
-		// TODO Auto-generated method stub
-		int[][] st = { { -1, -1, -1, 1, -1, -1, -1 }, { 20, 21, 22, 2, 3, 4, 5 }, { 19, -1, -1, 23, -1, -1, 6 },
-				{ 18, -1, -1, -1, -1, -1, 7 }, { 17, -1, -1, 13, -1, -1, 8 }, { 16, 15, 14, 12, 11, 10, 9 } };
-		int pos;
-
-		for (int i = 0; i < 6; i++) {
-			for (int j = 0; j < 7; j++) {
-				pos = st[i][j];
-				if (pos == -1) {
-					System.out.print(String.format("%30s", ""));
-				} else {
-					System.out.print(String.format("%30s", vec_States.get(pos - 1).toString()));
-				}
-			}
-			System.out.println("");
-		}
-	}
 
 	@SuppressWarnings("null")
 	private static void InitializeGrid() {
@@ -555,6 +462,10 @@ public class Main {
 		double prob_next = 0.75;
 		double prob_stay = 0.10;
 		double prob_overshoot = 0.15;
+
+		LoseReward = -100;
+		GoalReward = 10;
+		gamma = 0.99;
 
 		Vector<Directions> vec_directions = new Vector<Directions>();
 		Directions e = new Directions();
@@ -974,7 +885,7 @@ public class Main {
 		e.setOvershoot(false);
 		vec_directions.add(e);
 		e = new Directions();
-		vec_States.add(new State(13, vec_directions, 10));
+		vec_States.add(new State(13, vec_directions, GoalReward));
 
 		// STATE 14 - moves left, left2x, right, rigth2x, nothing
 		vec_directions = new Vector<Directions>();
@@ -1281,7 +1192,7 @@ public class Main {
 		e.setOvershoot(false);
 		vec_directions.add(e);
 		e = new Directions();
-		vec_States.add(new State(23, vec_directions, -100));
+		vec_States.add(new State(23, vec_directions, LoseReward));
 
 	}
 
